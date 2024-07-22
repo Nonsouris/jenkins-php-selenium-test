62a196a732b3592f72265034e4b497f892de91ec
Non <t0016029b@gmail.com>
2024-07-22 16:14:22 +0800

    jenkins

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,7 +2,7 @@
 
 set -x
 echo "Current working directory: $(pwd -P)"
-docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace php:7.2-apache
+docker run -d -p 80:80 --name my-apache-php-app -v /var/jenkins_home/workspace/jenkins-php-selenium-test/src php:7.2-apache
 sleep 1
 set +x
 
